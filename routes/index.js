14ab4d452af3f45ab9a1101ae057ce4daff9037b
dimitermusic <dimiteryyordanov@gmail.com>
2021-11-30 02:23:06 -0800

    updated h1 from / to /api

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Add a '/' then one of the following to view data from MySQL Database in JSON format:<br>'categories', 'products', 'tags'</h1>")
+  res.send("<h1>Add a '/api/' then one of the following to view data from MySQL Database in JSON format:<br>'categories', 'products', 'tags'</h1>")
 });
 
 module.exports = router;
